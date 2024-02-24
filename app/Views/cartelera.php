c0f9c072a6b34c3d975548f4b45db48a79e2475d
Rogelio Trujillo Martinez <rogeliotrujillo0@gmail.com>
2024-02-23 18:39:19 -0600

    Se corrigieron los estilos

--- app/Views/cartelera.php
+++ app/Views/cartelera.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <title>CINE</title>
-    <link rel="stylesheet" href="css/catalogoestilos.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/catalogoestilos.css">
     <script src="javaScript/Scripts.js"></script>
 </head>
 
