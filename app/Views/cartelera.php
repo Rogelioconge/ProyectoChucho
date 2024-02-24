d8c70ee2c7448afb4f3eb567e2afadd0c331d106
Rogelio Trujillo Martinez <rogeliotrujillo0@gmail.com>
2024-02-23 19:50:47 -0600

    Se agregaron estilos para el modal view, y se quito la parte del formulario

--- app/Views/cartelera.php
+++ app/Views/cartelera.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="css/catalogoestilos.css">
-    <script src="javaScript/Scripts.js"></script>
 </head>
 
 <body>
@@ -51,7 +50,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form id="purchaseForm" action="<?php echo base_url('Cine/calcularPrecio'); ?>" method="post">
+                    <!-- <form id="purchaseForm" action="<?php //echo base_url('Cine/calcularPrecio'); ?>" method="post"> -->
                         <label for="nombre">Nombre:</label>
                         <input type="text" id="nombre" name="nombre" required><br>
                         <label for="entradas">Entradas:</label>
@@ -62,7 +61,7 @@
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                             <button type="submit" class="btn btn-primary" id="submitPurchase">Comprar</button>
                         </div>
-                    </form>
+                    <!-- </form> -->
 
                 </div>
             </div>
@@ -74,6 +73,10 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
 
+
 </body>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="javaScript/Scripts.js"></script>
 </html>
